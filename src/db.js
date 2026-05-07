3a8c78c20de6753e27faad4d8dc608ce139609f0
f4bio2017-a11y <f4bio2017@gmail.com>
2026-05-07 16:39:18 +0700

    Create db.js

---
+++ src/db.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const sql = require('mssql');
+
+const config = {
+  server: process.env.SQL_SERVER || '',
+  database: process.env.SQL_DATABASE || '',
+  user: process.env.SQL_USER || '',
+  password: process.env.SQL_PASSWORD || '',
+  options: {
+    encrypt: true,
+    trustServerCertificate: false,
+    enableArithAbort: true,
+  },
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+};
+
+let pool = null;
+
+async function getPool() {
+  if (!pool) {
+    pool = await sql.connect(config);
+  }
+  return pool;
+}
+
+async function query(queryString, params) {
+  const p = await getPool();
+  const request = p.request();
+  if (params) {
+    for (const [key, value] of Object.entries(params)) {
+      request.input(key, value);
+    }
+  }
+  return request.query(queryString);
+}
+
+module.exports = { sql, getPool, query };
